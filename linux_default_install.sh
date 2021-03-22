d79deea0a939ddd22533b5fc03a521816930381b
w4ts0n <47597136+Lukas2112@users.noreply.github.com>
2021-03-22 22:16:40 +0100

    Update linux_default_install.sh

--- linux_default_install.sh
+++ linux_default_install.sh
@@ -1,1 +1,2 @@
-
+sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y 
+sudo apt install -y vim vlc vscodium xampp dbeaver-ce wireshark obs-studio arduino veracrypt texlive-full wireguard git bleachbit keepassxc
